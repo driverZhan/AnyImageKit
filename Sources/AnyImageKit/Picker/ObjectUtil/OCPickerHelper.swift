8e555901ab42a6be05f4460ed7f95857eaad6995
yong.zhan <yong.zhan@melinked.com>
2023-01-17 10:57:41 +0800

    objc tools

--- Sources/AnyImageKit/Picker/ObjectUtil/OCPickerHelper.swift
+++ Sources/AnyImageKit/Picker/ObjectUtil/OCPickerHelper.swift
@@ -20,13 +20,19 @@ import Foundation
     case shared
 }
 
+@objc enum SelectedTapAction: NSInteger {
+    case browse
+    case pick
+    case edit
+}
+
 @objc public protocol OCPickerDelegate {
     func imagePickerDidCancel(_ picker: ImagePickerController)
     func imagePicker(_ picker: ImagePickerController, didFinishPicking result: OCPickerResult)
 }
 
-@available(iOS 2.0, *)
-@MainActor class OCPickerHelper: NSObject {
+
+class OCPickerHelper: NSObject {
     @objc open weak var pickerDelegate: OCPickerDelegate?
     
     private var options = PickerOptionsInfo()
@@ -91,7 +97,133 @@ import Foundation
     
     /// Album Options
     /// - Default: smart album + user create album
-    open var albumOptions: PickerAlbumOption = [.smart, .userCreated]
+    public var albumOptions: PickerAlbumOption = [.smart, .userCreated]
+    
+    
+    /// Select Options
+    /// - Default: Photo
+    /// - .photoLive and .photoGIF are subtype of .photo and will be treated as a photo when not explicitly indicated, otherwise special handling will be possible (playable & proprietary)
+    public var selectOptions: PickerSelectOption = [.photo]
+    
+    @objc public var videoEnable: Bool = false {
+        didSet {
+            let hasVideo: Bool = selectOptions.contains(.video)
+            if (hasVideo && !videoEnable) {
+                selectOptions.remove(.video)
+            }else if (!hasVideo && videoEnable){
+                selectOptions.insert(.video)
+            }
+            options.selectOptions = selectOptions
+        }
+    }
+    
+    @objc public var livePhotoEnable: Bool = false {
+        didSet {
+            let hasVideo: Bool = selectOptions.contains(.photoLive)
+            if (hasVideo && !livePhotoEnable) {
+                selectOptions.remove(.photoLive)
+            }else if (!hasVideo && livePhotoEnable){
+                selectOptions.insert(.photoLive)
+            }
+            options.selectOptions = selectOptions
+        }
+    }
+    
+    @objc public var gifEnable: Bool = false {
+        didSet {
+            let hasVideo: Bool = selectOptions.contains(.photoGIF)
+            if (hasVideo && !gifEnable) {
+                selectOptions.remove(.photoGIF)
+            }else if (!hasVideo && gifEnable){
+                selectOptions.insert(.photoGIF)
+            }
+            options.selectOptions = selectOptions
+        }
+    }
+    
+    @objc var selectionTapAction: SelectedTapAction = .browse {
+        didSet {
+            switch selectionTapAction {
+            case .browse:
+                options.selectionTapAction = .preview
+                break
+            case .pick:
+                options.selectionTapAction = .quickPick
+                break
+            case .edit:
+                options.selectionTapAction = .openEditor
+                break
+            }
+        }
+    }
+    
+    @objc public var preSelected: [String] = [] {
+        didSet {
+            options.preselectAssets = preSelected
+        }
+    }
+    
+    
+    
+    @objc public var enableDebugLog: Bool = false {
+        didSet {
+            options.enableDebugLog = enableDebugLog
+        }
+    }
+    
+#if ANYIMAGEKIT_ENABLE_EDITOR
+    /// Save edited asset when picker dismiss with success
+    /// - Default: true
+    public var saveEditedAsset: Bool = true {
+        didSet {
+            options.saveEditedAsset = saveEditedAsset
+        }
+    }
+    
+    @objc public var photoEditable: Bool = false {
+        didSet {
+            let editable = options.editorOptions.contains(.photo)
+            if (!editable && photoEditable){
+                options.editorOptions.insert(.photo)
+            }else if (editable && !photoEditable) {
+                options.editorOptions.remove(.photo)
+            }
+        }
+    }
+    
+    ///TODO
+    /*public*/ var videoEditable: Bool = false {
+        didSet {
+            let editable = options.editorOptions.contains(.video)
+            if (!editable && videoEditable){
+                options.editorOptions.insert(.video)
+            }else if (editable && !videoEditable) {
+                options.editorOptions.remove(.video)
+            }
+        }
+    }
+    
+    /// Editor photo option info items
+    public var editorPhotoOptions: EditorPhotoOptionsInfo = .init()
+    
+    /// Editor video option info items
+    /*public*/ var editorVideoOptions: EditorVideoOptionsInfo = .init()
+#endif
+    
+#if ANYIMAGEKIT_ENABLE_CAPTURE
+    /// Capture option info items
+    public var captureOptions: CaptureOptionsInfo = .init()
+#endif
+    
+#if ANYIMAGEKIT_ENABLE_EDITOR && ANYIMAGEKIT_ENABLE_CAPTURE
+    /// Use Same Editor Options In Capture
+    /// - Default: true
+    @objc public var useSameEditorOptionsInCapture: Bool = true {
+        didSet {
+            options.useSameEditorOptionsInCapture = useSameEditorOptionsInCapture
+        }
+    }
+#endif
     
     
     
